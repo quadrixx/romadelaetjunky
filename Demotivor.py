108db3f477c2291679e3f6ec2cb1e0136f3d8a94
vovan <nopudgethere@mail.ru>
2023-05-16 01:47:41 +0500

    created a dem. class

---
+++ Demotivor.py
@@ -0,0 +1,13 @@
+
+
+
+class Demotivator():
+    def __init__(self, lower_text, higher_text, pic_url):
+        self.lower_text = lower_text
+        self.higher_text = higher_text
+        self.pic_url = pic_url
+    def demotivate(self):
+        pass
+
+
+
