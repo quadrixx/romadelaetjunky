8d0636732649920645f6f674f94d886ba4eb9298
vovan <nopudgethere@mail.ru>
2023-05-16 01:42:49 +0500

    getting rid of the files

--- cog.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord.ext import commands
-
-
-ASDFAF
-class cogq(commands.Cog):
-    async def zxc(self):
-        pass
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import discord
 from discord.ext import commands
-from on_ready import OnReadyCog
+from on_ready import OnReadyCog, setup
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='j!', intents = intents)
 
-bot.add_cog(OnReadyCog(bot))
+
+
+@bot.event
+async def on_message(msg):
+    print(msg, msg.attachments[0])
 
 
 
 
-
-
-
+setup(bot)
 
 bot.run('MTEwNzM5NTkxNjY2MDQ5MDI0MA.GsW_Wi.BGdsMPplqRRcG9etNGi0jE9vhlS0qBNdFyIFGM')
