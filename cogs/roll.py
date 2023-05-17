40b4aabad43c1d014baaa71e5025102d917e281c
Roman <sabir244@mail.ru>
2023-05-18 01:37:51 +0500

    ROLL 1.0.1  RELEASED!!!!!

--- cogs/roll.py
+++ cogs/roll.py
@@ -8,7 +8,7 @@ class roll(commands.Cog):
     @commands.slash_command(name='roll')
     async def roll(self, ctx):
         number = randint(0, 100)
-        await ctx.respond("{} получает случайное число - {}".format(ctx.user.name, number))
+        await ctx.respond("{} получает случайное число (1-100): {}".format(ctx.user.name, number))
 
 def setup(bot):
     bot.add_cog(roll(bot))
