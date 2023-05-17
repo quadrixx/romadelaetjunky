da53a48f59440d5a5db00ffcca664f8adf5e626c
Roman <sabir244@mail.ru>
2023-05-18 01:32:02 +0500

    ROLL 1.0.0  RELEASED!!!!!

---
+++ cogs/roll.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+from random import randint
+
+
+class roll(commands.Cog):
+
+    @commands.slash_command(name='roll')
+    async def roll(self, ctx):
+        number = randint(0, 100)
+        await ctx.respond("{} получает случайное число - {}".format(ctx.user.name, number))
+
+def setup(bot):
+    bot.add_cog(roll(bot))
