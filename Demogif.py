2908e194b7b825bea207e669487fd60d1391c3fe
Roman <sabir244@mail.ru>
2023-05-26 17:18:23 +0500

    добавлена демотивация гифок

--- Demogif.py
+++ Demogif.py
@@ -1,20 +1,37 @@
 import requests
-def get_gif(name):  # PEP8: lower_case_names for functions
+from Demotivor import demotivate
+from PIL import Image, ImageDraw, ImageSequence
+import io
+def get_gif(name:str):  # PEP8: lower_case_names for functions
     #находит в теноре рандомную хуету вместо нужной гифки и возвращает её юрл
     key = 'AIzaSyAaxn_f4rZAW89i65kxc4jNxFDx414_gtU'
     result = requests.get(f'https://tenor.googleapis.com/v2/search?q={name}&key={key}&locale=ru_RU&limit=10')
     data = result.json()
     url = data["results"][0]['media_formats']['gif']["url"]
     return url
-def di(url):
+def di(url:str):
     # скачивает гифку по юрлу(без хуйни)
     img_data = requests.get(url).content
     with open('image.gif', 'wb') as handler:
         handler.write(img_data)
 
-def tenor_gif_handler(link):
+def tenor_gif_handler(link:str):
     #вырезает из дискордовской ссылки название гифки по которому её по идее можно найти через гет гиф
     #по факту хуй там плавал, он находит рандомную хуету
     gif_name = link[23:-13]
     return gif_name
 
+def demotivate_gif(upper_text, lower_text, gif_name:str):
+    #спихжено отсюда https://github.com/python-pillow/Pillow/issues/3128
+    im = Image.open(gif_name)
+    frames = []
+    for frame in ImageSequence.Iterator(im):
+        frame = demotivate(upper_text, lower_text, frame)
+        b = io.BytesIO()
+        frame.save(b, format="GIF")
+        frame = Image.open(b)
+        frames.append(frame)
+    frames[0].save('out.gif', save_all=True, append_images=frames[1:], duration=40, loop=0)
+
+
+demotivate_gif("venomancer reaction", 'venomancer reaction', "image.gif")
--- Demotivor.py
+++ Demotivor.py
@@ -20,58 +20,74 @@ def di(url):
         handler.write(img_data)
 
 
-def demotivate(upper_text, lower_text, pic_url):
-    di(pic_url)
-    filename = 'image.jpg'
-    with Image.open(filename) as img:
+def demotivate(upper_text, lower_text, file):
+    #тут теперь типа перегрузка
+    if type(file) == type("string"):
+        #если на вход подается стока то это юрл картинки
+        #это когда делаем демотиватор картинки
+        print("img from url")
+        di(file)
+        filename = "image.jpg"
+        img = Image.open(filename)
         img.load()
-        size = img.size  # размер картинки внутри демотиватора
-        # ниже вычисление размера всего демотиватора
-        width = int(size[0] + size[0] * 0.4)
-        height = int(size[1] + size[1] * 0.4)
-        while width > 2000 or height > 2000:
-            width = int(width/2)
-            height = int(height/2)
-        dem = Image.new("RGB", (width, height), "black")
-        draw = ImageDraw.Draw(dem)
+    else:
+        #если это не строка то это кадр гифки из Demogif
+        print("gif frame")
+        img = file
+    size = img.size  # размер картинки внутри демотиватора
+    # ниже вычисление размера всего демотиватора
+    width = int(size[0] + size[0] * 0.4)
+    height = int(size[1] + size[1] * 0.4)
+    while width > 2000 or height > 2000:
+        width = int(width/2)
+        height = int(height/2)
+    dem = Image.new("RGB", (width, height), "black")
+    draw = ImageDraw.Draw(dem)
 
-        # ниже координаты верхнего левого угла картинки внутри демотиватора
-        img_x = round(width / 2 - size[0] / 2)
-        img_y = round(height / 2 - size[1] / 2 - height / 10)
-        img_center = pic_center(img_x, img_y, size[0], size[1])
-        dem.paste(img, (img_x, img_y))
-        # ниже рисует белую рамку вокруг картинки
-        draw.rectangle(((img_x - 2, img_y - 2), (img_x + size[0] + 2, img_y + size[1] + 2)), fill=None, outline='white')
-        # дальше создание с текста
-        upper_font_size = round(width / (len(upper_text) * 0.5))
-        #файл со шрифтом должен быть в этой же папке
+    # ниже координаты верхнего левого угла картинки внутри демотиватора
+    img_x = round(width / 2 - size[0] / 2)
+    img_y = round(height / 2 - size[1] / 2 - height / 10)
+    img_center = pic_center(img_x, img_y, size[0], size[1])
+    dem.paste(img, (img_x, img_y))
+    # ниже рисует белую рамку вокруг картинки
+    draw.rectangle(((img_x - 2, img_y - 2), (img_x + size[0] + 2, img_y + size[1] + 2)), fill=None, outline='white')
+    # дальше создание с текста
+    upper_font_size = round(width / (len(upper_text) * 0.5))
+    #файл со шрифтом должен быть в этой же папке
+    upper_font = ImageFont.truetype("times.ttf", size=upper_font_size)
+    upper_text_size = text_size(upper_text, upper_font)
+
+    #ниже уменьшает шрифт чтобы текст если текст не влезает
+    while upper_text_size[1] > ((height - size[1] - img_y - upper_text_size[1] * 2)) or upper_text_size[0] > width:
+        upper_font_size = upper_font_size - 1
         upper_font = ImageFont.truetype("times.ttf", size=upper_font_size)
         upper_text_size = text_size(upper_text, upper_font)
+    #координаты на которых будет расположен текст(это не левый верхний угол(это ваще хз что))
+    upper_text_x = img_center[0] - (upper_text_size[0] / 2)
+    upper_text_y = img_y + size[1]*1.02
+    draw.text((upper_text_x, upper_text_y), text=upper_text, color=(255, 255, 255), font=upper_font)
 
-        #ниже уменьшает шрифт чтобы текст если текст не влезает
-        while upper_text_size[1] > ((height - size[1] - img_y - upper_text_size[1] * 2)) or upper_text_size[0] > width:
-            upper_font_size = upper_font_size - 1
-            upper_font = ImageFont.truetype("times.ttf", size=upper_font_size)
-            upper_text_size = text_size(upper_text, upper_font)
-        #координаты на которых будет расположен текст(это не левый верхний угол(это ваще хз что))
-        upper_text_x = img_center[0] - (upper_text_size[0] / 2)
-        upper_text_y = img_y + size[1]*1.02
-        draw.text((upper_text_x, upper_text_y), text=upper_text, color=(255, 255, 255), font=upper_font)
-
-        #размеры шрифтов высчитываются по формулам выведенным методом тыка(как и всё остальное)
-        lower_font_size = round((upper_font.size - round(upper_font.size / (2 + len(lower_text)*0.5))))
+    #размеры шрифтов высчитываются по формулам выведенным методом тыка(как и всё остальное)
+    lower_font_size = round((upper_font.size - round(upper_font.size / (2 + len(lower_text)*0.5))))
+    lower_font = ImageFont.truetype("times.ttf", size=lower_font_size)
+    lower_text_size = text_size(lower_text, lower_font)
+    #копия костыля сверху
+    while lower_text_size[1] > ((height - size[1] - img_y - lower_text_size[1] * 2)) or lower_text_size[0] > width:
+        lower_font_size = lower_font_size - 1
         lower_font = ImageFont.truetype("times.ttf", size=lower_font_size)
         lower_text_size = text_size(lower_text, lower_font)
-        #копия костыля сверху
-        while lower_text_size[1] > ((height - size[1] - img_y - lower_text_size[1] * 2)) or lower_text_size[0] > width:
-            lower_font_size = lower_font_size - 1
-            lower_font = ImageFont.truetype("times.ttf", size=lower_font_size)
-            lower_text_size = text_size(lower_text, lower_font)
-        lower_text_x = img_center[0] - (lower_text_size[0] / 2)
-        lower_text_y = upper_text_y + lower_text_size[1] + upper_text_size[1]/2
-        draw.text((lower_text_x, lower_text_y), text=lower_text, color=(255, 255, 255), font=lower_font)
+    lower_text_x = img_center[0] - (lower_text_size[0] / 2)
+    lower_text_y = upper_text_y + lower_text_size[1] + upper_text_size[1]/2
+    draw.text((lower_text_x, lower_text_y), text=lower_text, color=(255, 255, 255), font=lower_font)
+
+    if type(file) == type("string"):
+        #если был был юрл то картинка сохраняется
         dem.save('demotivated.png')
-        #dem.show()
-#link = "https://media.discordapp.net/attachments/830124399369126001/1106618492469391380/image.png?width=799&height=676"
+    else:
+        #если это был кадр из гифки то его демотивированная версия
+        #возвращается в демогиф
+        return dem
+    #dem.show()
+#link = "https://media.discordapp.net/attachments/1107399270404989099/1111386315246809138/image.png"
 #demotivate('dsf', "dxc", link)
 #комментарии для тестов
